d10746e555b0dbbb89884cc1f85d84b56e6d6a1b
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-11-29 03:27:34 +0000

    Convert to resteasy-reactive (#2)

--- src/main/kotlin/dev/stocky37/tierlists/api/base/MongoResourceApi.kt
+++ src/main/kotlin/dev/stocky37/tierlists/api/base/MongoResourceApi.kt
@@ -3,8 +3,8 @@ package dev.stocky37.tierlists.api.base
 import com.mongodb.ErrorCategory
 import com.mongodb.MongoWriteException
 import dev.stocky37.tierlists.core.base.ResourceService
+import io.smallrye.mutiny.Uni
 import javax.inject.Inject
-import javax.ws.rs.InternalServerErrorException
 import javax.ws.rs.NotFoundException
 import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.Response
@@ -13,27 +13,23 @@ abstract class MongoResourceApi<Resource : Any> : ResourceApi<Resource> {
 	@Inject
 	lateinit var svc: ResourceService<Resource>
 
-	override fun get(id: String): Resource {
-		return svc.get(id) ?: throw NotFoundException()
-	}
 
-	override fun list(): Collection<Resource> {
-		return svc.list()
-	}
+	override fun get(id: String): Uni<Resource> =
+		svc.get(id).onItem().ifNull().failWith(NotFoundException()).map { r -> r!! }
 
-	override fun create(resource: Resource): Resource {
+	override fun list(): Uni<List<Resource>> = svc.list()
+
+	override fun create(resource: Resource): Uni<Resource> {
 		try {
 			return svc.create(resource)
 		} catch (e: MongoWriteException) {
 			throw if (e.error.category == ErrorCategory.DUPLICATE_KEY) {
 				WebApplicationException(e.error.message, Response.Status.CONFLICT)
 			} else {
-				InternalServerErrorException(e)
+				e
 			}
 		}
 	}
 
-	override fun delete(id: String): Resource {
-		return svc.delete(id) ?: throw NotFoundException()
-	}
+	override fun delete(id: String): Uni<Void> = svc.delete(id)
 }
--- src/main/kotlin/dev/stocky37/tierlists/core/base/MongoEntityResourceService.kt
+++ src/main/kotlin/dev/stocky37/tierlists/core/base/MongoEntityResourceService.kt
@@ -1,39 +1,47 @@
 package dev.stocky37.tierlists.core.base
 
 import io.quarkus.mongodb.panache.kotlin.PanacheMongoEntityBase
-import io.quarkus.mongodb.panache.kotlin.PanacheMongoRepository
+import io.quarkus.mongodb.panache.kotlin.reactive.ReactivePanacheMongoRepository
+import io.smallrye.mutiny.Uni
 import org.bson.types.ObjectId
 
 
 abstract class MongoEntityResourceService<Resource : Any, Entity : PanacheMongoEntityBase>
 	: ResourceService<Resource> {
 
-	override fun get(id: String): Resource? {
-		val entity = repo().findById(ObjectId(id))
-		return if (entity == null) null else fromEntity(entity)
+	override fun get(id: String): Uni<Resource?> {
+		return findById(id).map(::fromEntityNullable)
 	}
 
-	override fun list(): List<Resource> {
-		return repo().findAll().list().map { e -> fromEntity(e) }
+	override fun list(): Uni<List<Resource>> {
+		return repo().findAll().list().map { list -> list.map(::fromEntity) }
 	}
 
-	override fun create(resource: Resource): Resource {
-		val entity = toEntity(resource)
-		repo().persist(entity)
-		return fromEntity(entity)
+	override fun create(resource: Resource): Uni<Resource> {
+		return repo().persist(toEntity(resource)).map(::fromEntity)
 	}
 
-	override fun delete(id: String): Resource? {
-		val entity = repo().findById(ObjectId(id))
-		if (entity == null) {
-			return entity
+	override fun delete(id: String): Uni<Void> {
+		return findById(id).onItem().ifNotNull().transformToUni { e -> repo().delete(e!!) }
+	}
+
+	open fun findById(id: String): Uni<Entity?> {
+		return repo().findById(ObjectId(id))
+	}
+
+	protected fun fromEntityNullable(entity: Entity?): Resource? {
+		return if (entity == null) {
+			null
+		} else {
+			fromEntity(entity)
 		}
-		repo().delete(entity)
-		return fromEntity(entity)
 	}
 
-	protected abstract fun repo(): PanacheMongoRepository<Entity>
+	protected abstract fun repo(): ReactivePanacheMongoRepository<Entity>
 
 	protected abstract fun toEntity(resource: Resource): Entity
+
 	protected abstract fun fromEntity(entity: Entity): Resource
+
+
 }
