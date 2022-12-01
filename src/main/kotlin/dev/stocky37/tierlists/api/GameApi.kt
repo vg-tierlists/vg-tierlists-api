e1551f9614cd782a48043d07f36b58a56ff63121
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-12-01 02:04:07 +0000

    Add swagger endpoint (#5)

--- src/main/kotlin/dev/stocky37/tierlists/api/GameApi.kt
+++ src/main/kotlin/dev/stocky37/tierlists/api/GameApi.kt
@@ -2,15 +2,21 @@ package dev.stocky37.tierlists.api
 
 import dev.stocky37.tierlists.model.Game
 import io.smallrye.mutiny.Uni
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import org.jboss.resteasy.reactive.RestPath
 import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 
+@Tag(name = "Games")
 interface GameApi {
 
 	@GET
 	fun get(@RestPath("gameId") id: String): Uni<Game?>
 
 	@DELETE
+	@APIResponse(
+		description = "Delete a game",
+	)
 	fun delete(@RestPath("gameId") id: String): Uni<Void>
 }
